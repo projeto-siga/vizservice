79b0e10439e4be026f33a28bf468331ea2ea6d50
Raphael Paiva <raphaeldpaiva@gmail.com>
2018-05-22 11:54:12 -0300

    Convertendo o projeto para vraptor 3

--- jfrjvizservice/src/main/java/br/jus/trf2/jf/segsap/JFRJGraphViz.java
+++ jfrjvizservice/src/main/java/br/jus/trf2/jf/segsap/JFRJGraphViz.java
@@ -11,11 +11,6 @@ import org.apache.commons.io.IOUtils;
 public class JFRJGraphViz {
 	private static final String[] DOT_COMMAND_LINUX = {"/usr/bin/dot", "-Tsvg"};
 	private static final String[] DOT_COMMAND_WINDOWS = {"C:/Program Files (x86)/Graphviz2.38/bin/dot.exe", "-Tsvg"};
-	private static final String DEFAULT_GRAPH = "digraph \"\"{ graph[tooltip=\"Tramitação\"]  \"T2SITI\"[shape=\"rectangle\"][label=\"SITI\"][tooltip=\"SUBSECRETARIA DE INFRAESTRUTURA DE TI\"]; \"T2DIVEC\"[shape=\"rectangle\"][label=\"DIVEC\"][tooltip=\"DIVISÃO DE EDUCAÇÃO CORPORATIVA\"]; \"T2STI\"[shape=\"rectangle\"][label=\"STI\"][tooltip=\"SECRETARIA DE TECNOLOGIA DA INFORMAÇÃO\"]; \"T2COABDA\" [shape=\"oval\"] [label=\"COABDA\"][color=\"red\"][tooltip=\"COORDENADORIA DE APLICAÇÕES E BANCO DE DADOS\"]; \"T2COABDA\" -> \"T2SITI\"[tooltip=\"Transferido em 19/04/18\"][label=\"1\"]; \"T2SITI\" -> \"T2STI\"[tooltip=\"Transferido em 19/04/18\"][label=\"2\"]; \"T2STI\" -> \"T2DIVEC\"[tooltip=\"Transferido em 24/04/18\"][label=\"3\"]; \"T2DIVEC\" -> \"T2STI\"[tooltip=\"Transferido em 24/04/18\"][label=\"4\"]; \"T2STI\" -> \"T2COABDA\"[tooltip=\"Transferido em 25/04/18\"][label=\"5\"]; }";
-	
-	public static byte[] generateGraph() throws IOException {
-		return generateGraph(DEFAULT_GRAPH);
-	}
 
 	public static byte[] generateGraph(String dotFormatGraph) throws IOException {
         Runtime rt = Runtime.getRuntime();
@@ -33,7 +28,7 @@ public class JFRJGraphViz {
         writer.flush();
         writer.close();
         
-        byte[] bytes = IOUtils.toByteArray(stdout);
+        byte[] bytes = IOUtils.toString(stdout, "UTF-8").getBytes();
         
         try {
 			p.waitFor();
--- jfrjvizservice/src/main/java/br/jus/trf2/jf/segsap/JFRJVizService.java
+++ jfrjvizservice/src/main/java/br/jus/trf2/jf/segsap/JFRJVizService.java
@@ -2,21 +2,22 @@ package br.jus.trf2.jf.segsap;
 
 import java.io.IOException;
 
-import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
-import br.com.caelum.vraptor.observer.download.ByteArrayDownload;
-import br.com.caelum.vraptor.observer.download.Download;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.interceptor.download.ByteArrayDownload;
+import br.com.caelum.vraptor.interceptor.download.Download;
 
-@Controller
+@Resource
 public class JFRJVizService {
-		
+	
+	private static final String DEFAULT_GRAPH = "digraph \"\"{ graph[tooltip=\"Tramitação\"]  \"T2SITI\"[shape=\"rectangle\"][label=\"SITI\"][tooltip=\"SUBSECRETARIA DE INFRAESTRUTURA DE TI\"]; \"T2DIVEC\"[shape=\"rectangle\"][label=\"DIVEC\"][tooltip=\"DIVISÃO DE EDUCAÇÃO CORPORATIVA\"]; \"T2STI\"[shape=\"rectangle\"][label=\"STI\"][tooltip=\"SECRETARIA DE TECNOLOGIA DA INFORMAÇÃO\"]; \"T2COABDA\" [shape=\"oval\"] [label=\"COABDA\"][color=\"red\"][tooltip=\"COORDENADORIA DE APLICAÇÕES E BANCO DE DADOS\"]; \"T2COABDA\" -> \"T2SITI\"[tooltip=\"Transferido em 19/04/18\"][label=\"1\"]; \"T2SITI\" -> \"T2STI\"[tooltip=\"Transferido em 19/04/18\"][label=\"2\"]; \"T2STI\" -> \"T2DIVEC\"[tooltip=\"Transferido em 24/04/18\"][label=\"3\"]; \"T2DIVEC\" -> \"T2STI\"[tooltip=\"Transferido em 24/04/18\"][label=\"4\"]; \"T2STI\" -> \"T2COABDA\"[tooltip=\"Transferido em 25/04/18\"][label=\"5\"]; }";
+	
 	@Get
 	@Path("/viz")
 	public Download viz() throws IOException {
-		byte[] image = generateImage();
-		return new ByteArrayDownload(image, "image/svg+xml", "graphic.jpg");
+		return postViz(DEFAULT_GRAPH);
 	}
 	
 	@Post
@@ -24,10 +25,6 @@ public class JFRJVizService {
 	public Download postViz(String dot) throws IOException {
 		byte[] image = generateImage(dot);
 		return new ByteArrayDownload(image, "image/svg+xml", "graphic.jpg");
-	}
-	
-	private byte[] generateImage() throws IOException {
-		return JFRJGraphViz.generateGraph();
 	}
 	
 	private byte[] generateImage(String dot) throws IOException {
